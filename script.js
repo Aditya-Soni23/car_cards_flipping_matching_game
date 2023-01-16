60330f481ab33b669e5af7012f4cd4034d104441
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2023-01-16 22:11:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+var correct_attempt= 0;
+document.getElementById("score").innerHTML = "Correct attempts made - "+correct_attempt;
+var wrong_attempts = 0;
+document.getElementById("score_wrong").innerHTML = "Wrong attempts made - "+wrong_attempts;
+var stage = 1;
+document.getElementById("stage").innerHTML = stage;
+
+
+function mod(){
+    if(wrong_attempts < 10){
+        document.getElementById("mood").innerHTML = "Excellent";
+    }
+    else if(wrong_attempts > 10){
+        document.getElementById("mood").innerHTML = "Good";}
+    
+}
+
+
+function plzplay(){
+    document.getElementById("play").play();
+}
+const cards = document.querySelectorAll('.card'); 
+let cardOne, cardTwo;
+let disableDeck = false;
+let matchedCard = 0; 
+
+
+
+function flipCard(e){ 
+    let clickedCard = e.target; //getting user clicked card
+
+    if(clickedCard !== cardOne && !disableDeck){ 
+        clickedCard.classList.add('flip');
+        
+
+        if(!cardOne){
+            return cardOne = clickedCard; //return the cardOne value to clickedCard
+        }
+        cardTwo = clickedCard;
+
+        disableDeck = true;
+
+        let cardOneImg = cardOne.querySelector('img').src, 
+        cardTwoImg = cardTwo.querySelector('img').src; 
+        matchCards(cardOneImg, cardTwoImg);
+
+       
+    }
+}
+
+function matchCards(img1, img2){ 
+
+    if(img1 === img2){ // if two cards img matched
+        matchedCard++;
+        correct_attempt = correct_attempt + 1;
+        document.getElementById("score").innerHTML = "Correct attempts made - "+correct_attempt; //increment matched value by one
+       
+        if(matchedCard == 8){ // if matched value is 8 that means user has matched all the cards
+
+            setTimeout(() => {
+                stage = stage + 1;
+                document.getElementById("stage").innerHTML = stage;  
+                return shuffleCard();   
+            }, 1200); //calling shuffleCard function after 1s
+        }
+
+        cardOne.removeEventListener('click', flipCard);
+        cardTwo.removeEventListener('click', flipCard);
+        cardOne = cardTwo = '';//7.4
+        return disableDeck = false;
+    }
+    else{
+        setTimeout(() => { // if two card not matched
+            cardOne.classList.add('shake');// adding shake class to both card after 400ms
+            cardTwo.classList.add('shake');
+            wrong_attempts = wrong_attempts + 1;
+            document.getElementById("score_wrong").innerHTML = "Wrong attempts made - "+wrong_attempts;
+        }, 400);
+
+        setTimeout(() => { // removing both shake and flip classes from the both card after 1.2s
+            cardOne.classList.remove('shake', 'flip');
+            cardTwo.classList.remove('shake', 'flip');
+            cardOne = cardTwo = '';//setting both card value to blank
+
+            disableDeck = false;
+           
+
+        }, 1200);
+    }
+}
+function shuffleCard(){
+    matchedCard = 0;
+    cardOne = cardTwo = "";
+
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]; //creating array of 16 items and each item is repeated twice
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);//sorting array item randomly
+
+    cards.forEach((card, index) => { 
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+
+        let imgTag = card.querySelector('img');
+        imgTag.src = `images/img-${arr[index]}.png`;
+    });
+}
+shuffleCard();
+
+
+cards.forEach(card => { // adding click event to all cards
+    card.addEventListener('click', flipCard); 
+});
